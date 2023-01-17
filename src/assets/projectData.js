88c9357fd190293bb7d3830e5a60678089347d8b
wook2onandon <babyldk@gmail.com>
2023-01-17 18:07:06 +0900

    add projects UI & add projectsData

---
+++ src/assets/projectData.js
@@ -0,0 +1,84 @@
+export const projectData = [
+  {
+    id: 0,
+    title: '포트폴리오 웹사이트',
+    subTitle: '2023.01 (1人 개인 프로젝트)',
+    detail:
+      '포트폴리오 용도로 제작한 웹사이트입니다. 지금 보고 있는 바로 이 웹사이트에 해당합니다. 순수 React로 개발을 완료하였습니다. 슬라이더는 React-Slick library를 사용하여 arrow버튼과 paging버튼을 커스텀하여 사용하였고 메뉴를 눌렀을때 스크롤을 이동시키기 위해 useMoveScroll이라는 custom hook을 만들어서 구현했습니다.',
+    features:
+      '간단한 자기소개, 인적 사항, 기술 스택, GitHub링크, 프로젝트 경험, 업무 경력기술',
+    github: 'github.com/wook2onandon/my-page',
+    url: 'wook-portfolio.com',
+    frontend: 'react, module css, react-slick',
+    backend: '',
+    database: '',
+    deployment: 'Netlify',
+    images: [
+      '/images/shoppy-home.png',
+      '/images/shoppy-home.png',
+      '/images/shoppy-home.png',
+      '/images/shoppy-home.png',
+    ],
+  },
+  {
+    id: 1,
+    title: 'Shoppy',
+    subTitle: '2023.01 (1人 개인 프로젝트)',
+    detail:
+      'react를 사용하여 일반적인 반응형 쇼핑몰을 구현했습니다. firebase를 사용하여 로그인기능과 상품정보를 가져오고 추가하는 기능을 만들었습니다. 로그인 할 경우 uuid를 firebase에 등록하고 운영자로 등록되어있는 uuid를 가진 계정으로 로그인 할 경우 상품을 추가할 수 있는 버튼을 분기처리해서 일반사용자에게는 노출되지 않도록 하였습니다. 또한 로그인을 하면 장바구니버튼이 표시되며 실시간으로 firebase와 통신하여 장구바구니에 상품을 추가하고 제거할 수 있습니다. react-query를 사용하여 상태관리를 용이하게 만들었고 tailwind css를 사용하여 css작업을 간편하게 했습니다.',
+    features:
+      '일반적인 쇼핑몰 기능, 장바구니 추가, 로그인 기능, 운영자계정 상품 추가',
+    github: 'github.com/wook2onandon/shoppy',
+    url: 'wook-shoppy.com',
+    frontend: 'react, tailwind css, react-query, uuid',
+    backend: 'firebase',
+    database: 'firebase',
+    deployment: 'Netlify',
+    images: [
+      '/images/shoppy-home.png',
+      '/images/shoppy-home.png',
+      '/images/shoppy-home.png',
+      '/images/shoppy-home.png',
+    ],
+  },
+  {
+    id: 2,
+    title: '유튜브 프로젝트',
+    subTitle: '2023.01 (1人 개인 프로젝트)',
+    detail:
+      'Youtube의 오픈API를 사용해 실제 Youtube에 등록되어있는 데이터를 가져와 보여주는 사이트 제작, 불필요한 UI를 제거하고 관련된 동영상 추천과 검색기능만 구현하였습니다.',
+    features: '추천 동영상, 관련된 동영상, 영상재생, 영상검색 기능',
+    github: 'github.com/wook2onandon/youtube',
+    url: 'wook-yotube.com',
+    frontend: 'react, tailwind css, react-query',
+    backend: 'Youtube API, Postman',
+    database: '',
+    deployment: 'Netlify',
+    images: [
+      '/images/shoppy-home.png',
+      '/images/shoppy-home.png',
+      '/images/shoppy-home.png',
+      '/images/shoppy-home.png',
+    ],
+  },
+  {
+    id: 3,
+    title: 'Todo List',
+    subTitle: '2023.01 (1人 개인 프로젝트)',
+    detail:
+      '할일을 적어두는 Todo List앱 입니다. 다크모드 기능이 있고 Todo List입력기능, 완료된 활동과 완료되지 않은 활동을 나누어 볼 수 있도록 제작. 또한 웹페이지에서 나가거나 새로고침 하더라도 유지 될 수 있도록 Local Storage에 저장 되로록 만들었습니다.',
+    features: 'Todo List입력, 삭제, 완료설정, 다크모드',
+    github: 'github.com/wook2onandon/todo',
+    url: 'wook-todolist.com',
+    frontend: 'react, tailwind css, react-query',
+    backend: '',
+    database: '',
+    deployment: 'Netlify',
+    images: [
+      '/images/shoppy-home.png',
+      '/images/shoppy-home.png',
+      '/images/shoppy-home.png',
+      '/images/shoppy-home.png',
+    ],
+  },
+];
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { projectData } from '../../assets/projectData';
 import Slide from '../Slide/Slide';
 import styles from './Projects.module.css';
 
@@ -7,74 +8,72 @@ export default function Projects({ elementRef }) {
     <div ref={elementRef} className={styles.container}>
       <div className={styles.title}>Project</div>
       <ul className={styles.listWrap}>
-        <li className={styles.list}>
-          <div className={styles.projectTitleWrap}>
-            <h2 className={styles.projectTitle}>포트폴리오 웹사이트</h2>
-            <p className={styles.projectSubTitle}>
-              2023.01 (1人 개인 프로젝트)
-            </p>
-          </div>
-          <div className={styles.projectItemWrap}>
-            <div className={styles.projectSliderWrap}>슬라이더</div>
-            {/* <Slide /> */}
-            <div className={styles.projectBox}>
-              <p className={styles.projectTextDetail}>
-                포트폴리오 용도로 제작한 웹사이트입니다. 지금 보고 있는 바로 이
-                <br />
-                웹사이트에 해당합니다. 이미 순수 React로 개발을 완료한
-                <br />
-                프로젝트였지만, Next.js의 학습을 위해 이를 Next.js로 다시
-                <br />
-                개발하였습니다. 그 과정에서 서버 사이드 렌더링을 함께 지원하는
-                <br />
-                Next.js의 위대함을 느낄 수 있었습니다. 또한, Vercel을 이용하여
-                <br />
-                정적 웹사이트의 Lean한 배포도 경험해볼 수 있었습니다.
-              </p>
-              <button className={styles.btn}>자세히보기</button>
-              <div className={styles.hr} />
-              <ul className={styles.projectDetailList}>
-                <li className={styles.projectDetailWrap}>
-                  <div className={styles.projectDetail}>주요기능</div>
-                  <div className={styles.projectDetailText}>
-                    간단한 자기소개, 인적 사항, 기술 스택, GitHub 및 티스토리
-                    링크, 프로젝트 경험, 업무 경력
-                  </div>
-                </li>
-                <li className={styles.projectDetailWrap}>
-                  <div className={styles.projectDetail}>GitHub</div>
-                  <div className={styles.projectDetailText}>
-                    github.com/wook2onandon/my-page
-                  </div>
-                </li>
-                <li className={styles.projectDetailWrap}>
-                  <div className={styles.projectDetail}>url</div>
-                  <div className={styles.projectDetailText}>
-                    wook-portfolio.com
-                  </div>
-                </li>
-                <li className={styles.projectDetailWrap}>
-                  <div className={styles.projectDetail}>Frontend</div>
-                  <div className={styles.projectDetailText}>
-                    react, module css, react-slick
-                  </div>
-                </li>
-                <li className={styles.projectDetailWrap}>
-                  <div className={styles.projectDetail}>backend</div>
-                  <div className={styles.projectDetailText}> 없음</div>
-                </li>
-                <li className={styles.projectDetailWrap}>
-                  <div className={styles.projectDetail}>database</div>
-                  <div className={styles.projectDetailText}>없음</div>
-                </li>
-                <li className={styles.projectDetailWrap}>
-                  <div className={styles.projectDetail}>Deployment</div>
-                  <div className={styles.projectDetailText}>Netlify</div>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </li>
+        {projectData.map((data) => {
+          return (
+            <li className={styles.list} key={data.id}>
+              <div className={styles.projectTitleWrap}>
+                <h2 className={styles.projectTitle}>{data.title}</h2>
+                <p className={styles.projectSubTitle}>{data.subTitle}</p>
+              </div>
+              <div className={styles.projectItemWrap}>
+                <Slide slider={data.images} />
+                <div className={styles.projectBox}>
+                  <p className={styles.projectTextDetail}>{data.detail}</p>
+                  <button className={styles.btn}>자세히보기</button>
+                  <div className={styles.hr} />
+                  <ul className={styles.projectDetailList}>
+                    <li className={styles.projectDetailWrap}>
+                      <div className={styles.projectDetail}>주요기능</div>
+                      <div className={styles.projectDetailText}>
+                        {data.features}
+                      </div>
+                    </li>
+                    <li className={styles.projectDetailWrap}>
+                      <div className={styles.projectDetail}>GitHub</div>
+                      <div className={styles.projectDetailText}>
+                        {data.github}
+                      </div>
+                    </li>
+                    <li className={styles.projectDetailWrap}>
+                      <div className={styles.projectDetail}>url</div>
+                      <div className={styles.projectDetailText}>{data.url}</div>
+                    </li>
+                    <li className={styles.projectDetailWrap}>
+                      <div className={styles.projectDetail}>Frontend</div>
+                      <div className={styles.projectDetailText}>
+                        {data.frontend}
+                      </div>
+                    </li>
+                    {data.backend && (
+                      <li className={styles.projectDetailWrap}>
+                        <div className={styles.projectDetail}>Backend</div>
+                        <div className={styles.projectDetailText}>
+                          {data.backend}
+                        </div>
+                      </li>
+                    )}
+                    {data.database && (
+                      <li className={styles.projectDetailWrap}>
+                        <div className={styles.projectDetail}>Database</div>
+                        <div className={styles.projectDetailText}>
+                          {data.database}
+                        </div>
+                      </li>
+                    )}
+                    {data.deployment && (
+                      <li className={styles.projectDetailWrap}>
+                        <div className={styles.projectDetail}>Deployment</div>
+                        <div className={styles.projectDetailText}>
+                          {data.deployment}
+                        </div>
+                      </li>
+                    )}
+                  </ul>
+                </div>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
